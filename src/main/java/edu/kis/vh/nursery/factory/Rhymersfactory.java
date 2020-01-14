54f7b17b7eb78945b4e07888ab76477047ff07be
209400 <majkel43m@wp.pl>
2020-01-14 13:48:53 +0100

    formatowanie poprwawione według mojego standardu

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.defaultCountingOutRhymer;
 
-	public interface Rhymersfactory {
+	public interface Rhymersfactory
+	{
 	
 		public defaultCountingOutRhymer GetStandardRhymer();
 		
