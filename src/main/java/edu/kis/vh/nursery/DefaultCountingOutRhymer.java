b6e95123e39365b190abf2dd24661a5303b3a192
209400 <majkel43m@wp.pl>
2020-01-28 14:13:30 +0100

    poprwaki stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,46 +6,47 @@ public class DefaultCountingOutRhymer
 
     public static final int MAX_AMOUNT_OF_ELEMENTS = 12;
     public int EMPTY = -1;
+    public int AMOUNT_ELEMENTS = -1;
     private int[] NUMBERS = new int[MAX_AMOUNT_OF_ELEMENTS];
 
     public void countIn(int in)
     {
         if (!isFull())
-            NUMBERS[++EMPTY] = in;
+            NUMBERS[++AMOUNT_ELEMENTS] = in;
     }
 
     public boolean callCheck()
     {
-        return EMPTY == -1;
+        return AMOUNT_ELEMENTS == -1;
     }
 
     public boolean isFull()
     {
-        return EMPTY == 11;
+        return AMOUNT_ELEMENTS == 11;
     }
 
     protected int peekaboo()
     {
         if (callCheck())
             return EMPTY;
-        return NUMBERS[EMPTY];
+        return NUMBERS[AMOUNT_ELEMENTS];
     }
 
     public int countOut()
     {
         if (callCheck())
             return EMPTY;
-        return NUMBERS[EMPTY--];
+        return NUMBERS[AMOUNT_ELEMENTS--];
     }
 
     public int getTotal()
     {
-        return EMPTY;
+        return AMOUNT_ELEMENTS;
     }
 
     public int setTotal(int total)
     {
-        this.EMPTY = total;
-        return this.EMPTY;
+        this.AMOUNT_ELEMENTS = total;
+        return this.AMOUNT_ELEMENTS;
     }
 }
