0014e1d816af481b025c41d35227a7e6685d43f0
209400 <majkel43m@wp.pl>
2020-01-28 14:24:22 +0100

    poprwaki stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,48 +5,49 @@ public class DefaultCountingOutRhymer
 {
 
     public static final int MAX_AMOUNT_OF_ELEMENTS = 12;
-    public int EMPTY = -1;
-    public int AMOUNT_ELEMENTS = -1;
-    private int[] NUMBERS = new int[MAX_AMOUNT_OF_ELEMENTS];
+    public static final int MAX_INDEX = MAX_AMOUNT_OF_ELEMENTS - 1;
+    public final int EMPTY = -1;
+    public int amount_elements = -1;
+    private int[] numbers = new int[MAX_AMOUNT_OF_ELEMENTS];
 
     public void countIn(int in)
     {
         if (!isFull())
-            NUMBERS[++AMOUNT_ELEMENTS] = in;
+            numbers[++amount_elements] = in;
     }
 
     public boolean callCheck()
     {
-        return AMOUNT_ELEMENTS == -1;
+        return amount_elements == EMPTY;
     }
 
     public boolean isFull()
     {
-        return AMOUNT_ELEMENTS == 11;
+        return amount_elements == MAX_INDEX;
     }
 
     protected int peekaboo()
     {
         if (callCheck())
             return EMPTY;
-        return NUMBERS[AMOUNT_ELEMENTS];
+        return numbers[amount_elements];
     }
 
     public int countOut()
     {
         if (callCheck())
             return EMPTY;
-        return NUMBERS[AMOUNT_ELEMENTS--];
+        return numbers[amount_elements--];
     }
 
     public int getTotal()
     {
-        return AMOUNT_ELEMENTS;
+        return amount_elements;
     }
 
     public int setTotal(int total)
     {
-        this.AMOUNT_ELEMENTS = total;
-        return this.AMOUNT_ELEMENTS;
+        this.amount_elements = total;
+        return this.amount_elements;
     }
 }
