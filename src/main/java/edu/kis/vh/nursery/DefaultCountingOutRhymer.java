a75d4524a7baa69e55aac4189eb8e9468f17bda9
PrezesMajkel <prezesmajkel43@gmail.com>
2020-02-01 23:03:17 +0100

    wykonałem tyle zadań ile potraiłem i jak potrafiłem z cześci pierwszej

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,38 +1,60 @@
 package edu.kis.vh.nursery;
 
-
+/**
+ * Lista reprezentująca odliczankę
+ */
 public class DefaultCountingOutRhymer
 {
 
-    public static final int MAX_AMOUNT_OF_ELEMENTS = 12;
-    public static final int MAX_INDEX = MAX_AMOUNT_OF_ELEMENTS - 1;
-    public final int EMPTY = -1;
-    public int amount_elements = -1;
+    private static final int MAX_AMOUNT_OF_ELEMENTS = 12;
+    private static final int MAX_INDEX = MAX_AMOUNT_OF_ELEMENTS - 1;
+    private final int EMPTY = -1;
+    private int amount_elements = -1;
     private int[] numbers = new int[MAX_AMOUNT_OF_ELEMENTS];
 
+    /**
+     *  Dodanie nowego elementu
+     * @param in wartość nowego elementu
+     */
     public void countIn(int in)
     {
         if (!isFull())
             numbers[++amount_elements] = in;
     }
 
-    public boolean callCheck()
+    /**
+     *  Sprawdzenie, czy lista jest pust
+     * @return TRUE gdy lista jest pust, FALSE gdy lista nie jest pusta
+     */
+    boolean callCheck()
     {
         return amount_elements == EMPTY;
     }
 
-    public boolean isFull()
+    /**
+     *  Sprawdzenie czy lista jest pełna
+     * @return wartość logiczna TRUE, gdy lista jest pełna
+     */
+    boolean isFull()
     {
         return amount_elements == MAX_INDEX;
     }
 
-    protected int peekaboo()
+    /**
+     *  Wyłuskanie wartości ostatniego elementu listy
+     * @return wartość ostatniego elementu listy, lub -1 jeżeli lista była pusta
+     */
+    int peekaboo()
     {
         if (callCheck())
             return EMPTY;
         return numbers[amount_elements];
     }
 
+    /**
+     *  Wyłuskanie wartości przedostatniego elementu listy
+     * @return wartość przedostatniego elementu listy, lub -1 jeżeli lista była pusta
+     */
     public int countOut()
     {
         if (callCheck())
@@ -40,14 +62,13 @@ public class DefaultCountingOutRhymer
         return numbers[amount_elements--];
     }
 
+    /**
+     *  Ilość elementów listy
+     * @return ilość elementów listy
+     */
     public int getTotal()
     {
-        return amount_elements;
+        return amount_elements + 1;
     }
 
-    public int setTotal(int total)
-    {
-        this.amount_elements = total;
-        return this.amount_elements;
-    }
 }
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,12 +1,19 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Lista wiązana, dwukierunkowa, warości typu INT
+ */
 public class IntLinkedList
 {
 
-	public static final int MINUS = -1;
-	Node last;
+	private static final int MINUS = -1;
+	private Node last;
 	int i;
 
+    /**
+     *  Dodanie nowego elementu
+     * @param i wartość nowego elementu
+     */
 	public void push(int i)
 	{
 		if (last == null)
@@ -19,16 +26,28 @@ public class IntLinkedList
 		}
 	}
 
-	public boolean isEmpty()
+    /**
+     *  Sprawdzenie czy lista jest pusta
+     * @return
+     */
+	private boolean isEmpty()
 	{
 		return last == null;
 	}
 
+    /**
+     *  Sprawdzenie czy lista jest pełna
+     * @return wartość logiczna FALSE, gdy lista nie jest pełna
+     */
 	public boolean isFull()
 	{
 		return false;
 	}
 
+    /**
+     *  Wyłuskanie wartości ostatniego elementu listy
+     * @return wartość ostatniego elementu listy, lub -1 jeżeli lista była pusta
+     */
 	public int top()
 	{
 		if (isEmpty())
@@ -36,6 +55,10 @@ public class IntLinkedList
 		return last.value;
 	}
 
+    /**
+     *  Zdjęcie ostatniego elementu listy; przedostatni element staje się ostatnim
+     * @return wartość ostatniego elementu listy, lub -1 jeżeli lista była pusta
+     */
 	public int pop()
 	{
 		if (isEmpty())
